e28a6d8ab1c74972014fd9626f08104bc8b684c3
Nierot <56843328+Nierot@users.noreply.github.com>
2020-11-06 15:35:53 +0100

    Standard express server

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const app = require('express')();
-const { API_TOKEN, PORT } = require('./settings.json');
+const { API_TOKEN, PORT, ROUTE } = require('./settings.json');
+
+app.get(`${ROUTE}/`, (req, res) => {
+    res.send('Youtube-lookup-id API')
+});
+
+app.get(`${ROUTE}/id`, (req, res) => {
+    res.send('id');
+});
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}${ROUTE}`))
