2c578282fa1d8b508800d19a4d8cf861ce06853a
Nierot <56843328+Nierot@users.noreply.github.com>
2020-11-06 15:59:52 +0100

    Basic implementation

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const app = require('express')();
+const YouTube = require('simple-youtube-api');
 const { API_TOKEN, PORT, ROUTE } = require('./settings.json');
 
-app.get(`${ROUTE}/`, (req, res) => {
-    res.send('Youtube-lookup-id API')
+// Setup YouTube
+const yt = new YouTube(API_TOKEN);
+
+// Define Routes
+app.get(`${ROUTE}/info`, (req, res) => {
+    const id = req.query.id;
+    if (!id) return res.status(400).json({
+        code: 400, 
+        status: 'id paramater missing'
+    });
+
+    yt.getVideoByID(id)
+        .then(data => res.status(200).send(data))
+        .catch(err => res.status(500).send(err))
 });
 
-app.get(`${ROUTE}/id`, (req, res) => {
-    res.send('id');
-});
 
+// Start server
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}${ROUTE}`))
